12b52aa9db32361525381dabf5aa55bdeffec976
YokagiTosho <sandascend@gmail.com>
2025-07-26 21:32:20 +0300

    Implement simple websocket chat without proper protocol

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,81 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/json/src.hpp>
+
 #include <cstdlib>
-#include <functional>
 #include <iostream>
 #include <string>
 #include <thread>
+#include <memory>
 
 #include "blocking_queue.hpp"
 
-namespace beast = boost::beast;         // from <boost/beast.hpp>
-namespace http = beast::http;           // from <boost/beast/http.hpp>
-namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
-namespace net = boost::asio;            // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
 
 //------------------------------------------------------------------------------
 
-// Echoes back all received WebSocket messages
+class Message {
+    beast::flat_buffer m_buffer;
+public:
+    void read_message(websocket::stream<tcp::socket> &ws) {
+        ws.read(m_buffer);
+    }
+
+    Message() {}
+    Message(Message &&msg) : m_buffer(std::move(msg.m_buffer)) {}
+    Message(const Message &msg) : m_buffer(msg.m_buffer) {}
+
+    auto data() const {
+        return m_buffer.data();
+    }
+
+    friend std::ostream &operator<<(std::ostream& os, const Message& msg)
+    {
+        os << beast::buffers_to_string(msg.m_buffer.data());
+        return os;
+    }
+};
+
+class User {
+private:
+    std::shared_ptr<websocket::stream<tcp::socket>> m_ws;
+    std::string m_name;
+    // TODO add 'join_time'
+public:
+    User(std::shared_ptr<websocket::stream<tcp::socket>> ws)
+        : m_ws(std::move(ws)), m_name("Anon") {std::cout << "in User cntr, " << m_ws.use_count() << std::endl; }
+
+    User(std::shared_ptr<websocket::stream<tcp::socket>> ws, std::string name)
+        : m_ws(ws), m_name(name) {}
+
+    User(const User &user)
+        : m_ws(user.m_ws), m_name(user.m_name) {}
+
+    User(User &&user) : m_ws(std::move(user.m_ws)), m_name(user.m_name) {}
+
+    void send_message(const Message &msg) {
+        m_ws->text(m_ws->got_text());
+        m_ws->write(msg.data());
+    }
+};
+
+std::vector<User> users;
+BlockQueue<Message> message_queue; // thread safe blocking queue
+
 void
 do_session(tcp::socket socket)
 {
     try
     {
-        // Construct the stream by moving in the socket
-        websocket::stream<tcp::socket> ws{std::move(socket)};
+        auto ws = std::make_shared<websocket::stream<tcp::socket>>(std::move(socket));
 
         // Set a decorator to change the Server of the handshake
-        ws.set_option(websocket::stream_base::decorator(
+        ws->set_option(websocket::stream_base::decorator(
             [](websocket::response_type& res)
             {
                 res.set(http::field::server,
@@ -36,30 +84,39 @@ do_session(tcp::socket socket)
             }));
 
         // Accept the websocket handshake
-        ws.accept();
+        ws->accept();
+
+        users.push_back(ws);
 
         for(;;)
         {
-            // This buffer will hold the incoming message
-            beast::flat_buffer buffer;
+            Message msg;
+            msg.read_message(*ws);
 
-            // Read a message
-            ws.read(buffer);
-
-            // Echo the message back
-            ws.text(ws.got_text());
-            ws.write(buffer.data());
+            message_queue.push(std::move(msg));
         }
     }
     catch(beast::system_error const& se)
     {
-        // This indicates that the session was closed
         if(se.code() != websocket::error::closed)
             std::cerr << "Error: " << se.code().message() << std::endl;
     }
     catch(std::exception const& e)
     {
         std::cerr << "Error: " << e.what() << std::endl;
+    }
+}
+
+// run in separate thread, pop messages from the queue
+// and broadcast to all users
+void handle_messages() {
+    for (;;) {
+        auto msg = message_queue.pop();
+        std::cout << msg;
+
+        for (auto &user : users) {
+            user.send_message(msg);
+        }
     }
 }
 
@@ -73,28 +130,26 @@ int main(int argc, char* argv[])
         if (argc != 3)
         {
             std::cerr <<
-                "Usage: websocket-server-sync <address> <port>\n" <<
+                "Usage: ws-chat <address> <port>\n" <<
                 "Example:\n" <<
-                "    websocket-server-sync 0.0.0.0 8080\n";
+                "    ws-chat 127.0.0.1 8080\n";
             return EXIT_FAILURE;
         }
         auto const address = net::ip::make_address(argv[1]);
         auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
 
-        // The io_context is required for all I/O
         net::io_context ioc{1};
 
-        // The acceptor receives incoming connections
         tcp::acceptor acceptor{ioc, {address, port}};
+
+        std::thread(handle_messages).detach();
+
         for(;;)
         {
-            // This will receive the new connection
             tcp::socket socket{ioc};
 
-            // Block until we get a connection
             acceptor.accept(socket);
 
-            // Launch the session, transferring ownership of the socket
             std::thread(
                 &do_session,
                 std::move(socket)).detach();
