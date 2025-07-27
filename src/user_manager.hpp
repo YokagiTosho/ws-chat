f5660fd3399c9220bc501e3c903876da887173fc
YokagiTosho <sandascend@gmail.com>
2025-07-27 13:31:17 +0300

    Make UserManager iterable and thread safe

--- src/user_manager.hpp
+++ src/user_manager.hpp
@@ -1,20 +1,47 @@
 #ifndef __USER_MANAGER_HPP
 #define __USER_MANAGER_HPP
 
+#include <iostream>
 #include <unordered_map>
 #include <mutex>
 
 #include "user.hpp"
 
-// TODO make class iterable and thread safe
+// TODO thread safe
 class UserManager {
 private:
-    std::unordered_map<size_t, User> m_users;
     std::mutex mtx;
+    std::unordered_map<size_t, User> m_users;
 public:
-    void erase(size_t id);
-    void insert(size_t id, User &&user);
-    void insert(size_t id, const User &user);
+    void erase(size_t id) {
+        std::lock_guard<std::mutex> lck(mtx);
+
+        m_users.erase(id);
+    }
+    void insert(size_t id, User &&user) {
+        std::lock_guard<std::mutex> lck(mtx);
+
+        m_users.insert({id, user});
+    }
+    void insert(size_t id, const User &user) {
+        std::lock_guard<std::mutex> lck(mtx);
+
+        std::cout << "const User &user" << std::endl;
+        m_users.insert({id, user});
+    }
+
+    size_t size() const {
+        return m_users.size();
+    }
+
+    auto begin() noexcept
+    { return m_users.begin(); }
+    auto cbegin() const noexcept
+    { return m_users.cbegin(); }
+    auto end() noexcept
+    { return m_users.end();  }
+    auto cend() const noexcept
+    { return m_users.cend(); }
 };
 
 #endif
