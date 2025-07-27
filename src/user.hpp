733ee4d8bceb95f4d6d45a34347903559acf3dd3
YokagiTosho <sandascend@gmail.com>
2025-07-27 13:32:36 +0300

    Add receive_message method to User, fix move cntr

--- src/user.hpp
+++ src/user.hpp
@@ -1,11 +1,8 @@
 #ifndef __USER_HPP
 #define __USER_HPP
 
-#include <iostream>
-
 #include "boost_includes.hpp"
 #include "message.hpp"
-
 
 class User {
 private:
@@ -14,10 +11,10 @@ private:
     std::string m_name;
     // TODO add 'join_time'
 public:
+
     User(std::shared_ptr<websocket::stream<tcp::socket>> ws, size_t id)
-        : m_ws(std::move(ws)), m_id(id), m_name("Anon")
+        : m_ws(ws), m_id(id), m_name("Anon")
     {
-        std::cout << "in User cntr, " << m_ws.use_count() << std::endl;
     }
 
     User(std::shared_ptr<websocket::stream<tcp::socket>> ws, size_t id, std::string name)
@@ -29,12 +26,20 @@ public:
     {}
 
     User(User &&user)
-        : m_ws(std::move(user.m_ws)), m_id(user.m_id), m_name(user.m_name)
+        : m_ws(std::move(user.m_ws)), m_id(user.m_id), m_name(std::move(user.m_name))
     {}
 
     void send_message(const Message &msg) {
         m_ws->text(m_ws->got_text());
         m_ws->write(msg.data());
+    }
+    
+    size_t id() { return m_id; }
+
+    Message receive_message() {
+        Message msg;
+        msg.read_message(*m_ws);
+        return msg;
     }
 };
 
